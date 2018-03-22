3a9d15e32fd8445cc0cb73b1d25927f3b66cc8b2
zhouhui <zhouh08@hotmail.com>
2018-03-22 11:00:45 +0800

    hystrix dashboard

--- eurekaclient/src/main/java/com/zhou/EurekaclientApplication.kt
+++ eurekaclient/src/main/java/com/zhou/EurekaclientApplication.kt
@@ -1,9 +1,13 @@
 package com.zhou
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+import com.netflix.ribbon.proxy.annotation.Hystrix
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,14 +15,21 @@ import org.springframework.web.bind.annotation.RestController
 @EnableEurekaClient
 @RestController
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 class EurekaclientApplication {
 
     @Value("\${server.port}")
     internal var port : String? = null
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     fun home(@RequestParam name : String) : String {
         return "hi $name,i am from port:$port"
+    }
+
+    fun hiError(name : String) : String {
+        return "hi,$name,sorry,error!"
     }
 }
 
